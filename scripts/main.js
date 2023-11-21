11094c736ac3be4f08767b9925af572fda1ee7db
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-11-22 00:36:38 +0800

    Added current section detection in the navbar.

--- scripts/main.js
+++ scripts/main.js
@@ -32,7 +32,53 @@ sidebarOptions.forEach((elem) => {
             if (iterations >= event.target.dataset.value.length) clearInterval(interval);
 
             iterations += 1;
-        }, 200)
+        }, 100)
     })
-})
+});
+
+// List of all sidebar anchor tags
+let homepageAnchor = document.querySelector(".homepage-anchor");
+let exploreAnchor = document.querySelector(".explore-anchor");
+let aboutAnchor = document.querySelector(".about-anchor");
+let engagementAnchor = document.querySelector(".engagement-anchor");
+let pricingAnchor = document.querySelector(".pricing-anchor");
+
+let removeAllActiveAnchor = () => {
+    homepageAnchor.classList.remove('active');
+    exploreAnchor.classList.remove('active');
+    aboutAnchor.classList.remove('active');
+    engagementAnchor.classList.remove('active');
+    pricingAnchor.classList.remove('active');
+}
+
+let checkScrollStatus = () => {
+    let scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    
+    if (scrollTop < window.innerHeight * 0.7 ) {
+        removeAllActiveAnchor();
+        homepageAnchor.classList.add('active');
+    }
+    else if (scrollTop < window.innerHeight * 1.7) {
+        removeAllActiveAnchor();
+        exploreAnchor.classList.add('active');
+    }
+    else if (scrollTop < window.innerHeight * 2.7) {
+        removeAllActiveAnchor();
+        aboutAnchor.classList.add('active');
+    }
+    else if (scrollTop < window.innerHeight * 3.7) {
+        removeAllActiveAnchor();
+        engagementAnchor.classList.add('active');
+    }
+    else if (scrollTop < window.innerHeight * 4.7) {
+        removeAllActiveAnchor();
+        pricingAnchor.classList.add('active');
+    }
+}
+
+checkScrollStatus(); 
+
+window.addEventListener('scroll', () => {
+    checkScrollStatus();
+});
 
